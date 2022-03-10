5177b1530a5c433e39aa8d872896009dc86dd2d8
Abhishek1632 <abhishekdaundkar4131@gmail.com>
2022-03-10 22:39:48 +0530

    Added Variables & Keywords

---
+++ Hello.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+int main() {
+   // printf() displays the string inside quotation
+
+
+   printf("Hello, World!");
+   return 0;
+
+
+//    int age = 22;
+// float pi = 3.14;
+// char percentage = '%';
+
+// printf("age is %d", age);
+// printf("age is %f", pi);
+// printf("age is %c", percentage);
+// return 0;
+
+//   // Sum of the two numbers
+// int a, b;
+// printf("enter a \n");
+// scanf("%d", &a);
+// printf("enter b \n");
+// return 0;
+
+// scanf("%d", &b);
+// printf("sum of a & b is : %d \n", a+b);
+// return 0;
+
+// // Square of the Side
+// int side;
+// scanf("%d", &side\n);
+// printf("%d", side * side);
+// return 0;
+
+   
+
+// // Area of the Circle
+//    float radius;
+// scanf("%f", &radius);
+// printf("%f", 3.14 * radius * radius);
+// return 0;
+
+}
